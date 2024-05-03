70c9e868a31edd69534ff6cd254452a9c985b12d
Shinzen <83638956+Shinzen31@users.noreply.github.com>
2024-05-04 01:27:52 +0200

    Update solve.c

--- src/stencil/solve.c
+++ src/stencil/solve.c
@@ -1,38 +1,29 @@
 #include "stencil/solve.h"
-
 #include <assert.h>
 #include <math.h>
 
-void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
+void solve_jacobi(mesh_t* A, const mesh_t* B, mesh_t* C) {
     assert(A->dim_x == B->dim_x && B->dim_x == C->dim_x);
     assert(A->dim_y == B->dim_y && B->dim_y == C->dim_y);
     assert(A->dim_z == B->dim_z && B->dim_z == C->dim_z);
 
-    usz const dim_x = A->dim_x;
-    usz const dim_y = A->dim_y;
-    usz const dim_z = A->dim_z;
-    for (usz k = STENCIL_ORDER; k < dim_z - STENCIL_ORDER; ++k) {
-        for (usz j = STENCIL_ORDER; j < dim_y - STENCIL_ORDER; ++j) {
-            for (usz i = STENCIL_ORDER; i < dim_x - STENCIL_ORDER; ++i) {
-                C->cells[i][j][k].value = A->cells[i][j][k].value * B->cells[i][j][k].value;
+    for (usz k = STENCIL_ORDER; k < C->dim_z - STENCIL_ORDER; ++k) {
+        for (usz j = STENCIL_ORDER; j < C->dim_y - STENCIL_ORDER; ++j) {
+            for (usz i = STENCIL_ORDER; i < C->dim_x - STENCIL_ORDER; ++i) {
+                *idx_core(C, i, j, k) = *idx_core(A, i, j, k) * idx_core_const(B, i, j, k);
 
+                // Apply stencil operation using values from A and B
                 for (usz o = 1; o <= STENCIL_ORDER; ++o) {
-                    C->cells[i][j][k].value += A->cells[i + o][j][k].value *
-                                               B->cells[i + o][j][k].value / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i - o][j][k].value *
-                                               B->cells[i - o][j][k].value / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i][j + o][k].value *
-                                               B->cells[i][j + o][k].value / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i][j - o][k].value *
-                                               B->cells[i][j - o][k].value / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i][j][k + o].value *
-                                               B->cells[i][j][k + o].value / pow(17.0, (f64)o);
-                    C->cells[i][j][k].value += A->cells[i][j][k - o].value *
-                                               B->cells[i][j][k - o].value / pow(17.0, (f64)o);
+                    *idx_core(C, i, j, k) += (
+                        (*idx_core(A, i + o, j, k) * idx_core_const(B, i + o, j, k)) +
+                        (*idx_core(A, i - o, j, k) * idx_core_const(B, i - o, j, k)) +
+                        (*idx_core(A, i, j + o, k) * idx_core_const(B, i, j + o, k)) +
+                        (*idx_core(A, i, j - o, k) * idx_core_const(B, i, j - o, k)) +
+                        (*idx_core(A, i, j, k + o) * idx_core_const(B, i, j, k + o)) +
+                        (*idx_core(A, i, j, k - o) * idx_core_const(B, i, j, k - o))
+                    ) / pow(17.0, (f64)o);
                 }
             }
         }
     }
-
-    mesh_copy_core(A, C);
 }
