3637b4149df8e5d07380545fddebe8eb58fea6e3
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-04 23:36:40 +0200

    index

--- src/stencil/solve.c
+++ src/stencil/solve.c
@@ -19,13 +19,18 @@ void solve_jacobi(mesh_t* A, const mesh_t* B, mesh_t* C) {
     }
 
     usz ghost_size = 2 * STENCIL_ORDER;
-    usz cell_count = (A->dim_x - ghost_size) * (A->dim_y - ghost_size) * (A->dim_z - ghost_size);
+    usz cell_count = A->dim_x * A->dim_y * A->dim_z;
     
     #pragma omp parallel for
     for (usz ind = 0; ind < cell_count; ++ind) {
         usz i = (ind / ((A->dim_y - ghost_size) * (A->dim_z - ghost_size))) + STENCIL_ORDER;
         usz j = ((ind / (A->dim_z - ghost_size)) % (A->dim_y - ghost_size)) + STENCIL_ORDER;
         usz k = (ind % (A->dim_z - ghost_size)) + STENCIL_ORDER;
+
+        if (A->cells[ind].kind != CELL_KIND_CORE) {
+            continue;
+        }
+        
         f64 sum = idx_core_const(A, i, j, k) * idx_core_const(B, i, j, k);
 
         for (usz o = 1; o <= STENCIL_ORDER; ++o) {
