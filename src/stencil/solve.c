061da74c54339586c1f40f9dfe513f2db59f2e59
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-04 15:20:42 +0200

    bound condition

--- src/stencil/solve.c
+++ src/stencil/solve.c
@@ -12,6 +12,7 @@ void solve_jacobi(mesh_t* A, const mesh_t* B, mesh_t* C) {
     omp_set_num_threads(24);
 
     f64 precomputed_powers[STENCIL_ORDER + 1];
+    precomputed_powers[0] = 1.0;
     #pragma omp parallel for
     for (int i = 1; i <= STENCIL_ORDER; i++) {
         precomputed_powers[i] = pow(17.0, (f64)i);
@@ -24,11 +25,16 @@ void solve_jacobi(mesh_t* A, const mesh_t* B, mesh_t* C) {
         usz i = (ind / ((A->dim_y - ghost_size) * (A->dim_z - ghost_size))) + STENCIL_ORDER;
         usz j = ((ind / (A->dim_z - ghost_size)) % (A->dim_y - ghost_size)) + STENCIL_ORDER;
         usz k = (ind % (A->dim_z - ghost_size)) + STENCIL_ORDER;
-        *idx_core(C, i, j, k) = *idx_core(A, i, j, k) * idx_core_const(B, i, j, k);
 
-        // Apply stencil operation using values from A and B
-        for (usz o = 1; o <= STENCIL_ORDER; ++o) {
-            *idx_core(C, i, j, k) += (
+        if (i >= STENCIL_ORDER && i < A->dim_x - STENCIL_ORDER &&
+            j >= STENCIL_ORDER && j < A->dim_y - STENCIL_ORDER &&
+            k >= STENCIL_ORDER && k < A->dim_z - STENCIL_ORDER) {
+                
+            *idx_core(C, i, j, k) = *idx_core(A, i, j, k) * idx_core_const(B, i, j, k);
+
+            // Apply stencil operation using values from A and B
+            for (usz o = 1; o <= STENCIL_ORDER; ++o) {
+                *idx_core(C, i, j, k) += (
                         (*idx_core(A, i + o, j, k) * idx_core_const(B, i + o, j, k)) +
                         (*idx_core(A, i - o, j, k) * idx_core_const(B, i - o, j, k)) +
                         (*idx_core(A, i, j + o, k) * idx_core_const(B, i, j + o, k)) +
@@ -36,6 +42,7 @@ void solve_jacobi(mesh_t* A, const mesh_t* B, mesh_t* C) {
                         (*idx_core(A, i, j, k + o) * idx_core_const(B, i, j, k + o)) +
                         (*idx_core(A, i, j, k - o) * idx_core_const(B, i, j, k - o))
                     ) / precomputed_powers[o];    
+            }
         }
     }
 }
