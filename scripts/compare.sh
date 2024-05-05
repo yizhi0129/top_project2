f994b3f7a3eefb44951634b72fe65bfc38c2a001
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 14:06:29 +0200

    1D index: speed up 13-30 times

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100.txt > ../speed_up/speed_up_100.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500.txt > ../speed_up/speed_up_500.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000.txt > ../speed_up/speed_up_1000.txt
+python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_24.txt > ../speed_up/speed_up_100_ind.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_24.txt > ../speed_up/speed_up_500_ind.txt
+python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_24.txt > ../speed_up/speed_up_1000_ind.txt
