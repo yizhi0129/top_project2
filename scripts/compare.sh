3d68439de4f84bdbca52002be306f20f11a36b91
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 22:39:46 +0200

    block 40 best

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for i in 1 2 4 8 16 24 48
+for i in 40 48 56
 do
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_-Ofast_$i.txt > ../speed_up/scal_100_$i.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_-Ofast_$i.txt > ../speed_up/scal_500_$i.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_-Ofast_$i.txt > ../speed_up/scal_1000_$i.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_block$i.txt > ../speed_up/speed_up_500_block$i.txt
 done
