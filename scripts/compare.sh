144b05b26dafcbea9044bd7f26731592612a6088
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 16:27:36 +0200

    best so far: -Ofast, block 32, 24 threads

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_24.txt > ../speed_up/speed_up_100_ind.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_24.txt > ../speed_up/speed_up_500_ind.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_24.txt > ../speed_up/speed_up_1000_ind.txt
+python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_-O3_24.txt > ../speed_up/speed_up_100_-O3.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_-O3_24.txt > ../speed_up/speed_up_500_-O3.txt
+python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_-O3_24.txt > ../speed_up/speed_up_1000_-O3.txt
+
+python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_-Ofast_24.txt > ../speed_up/speed_up_100_-Ofast.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_-Ofast_24.txt > ../speed_up/speed_up_500_-Ofast.txt
+python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_-Ofast_24.txt > ../speed_up/speed_up_1000_-Ofast.txt
+
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_block64.txt > ../speed_up/speed_up_500_block64.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_block32.txt > ../speed_up/speed_up_500_block32.txt
