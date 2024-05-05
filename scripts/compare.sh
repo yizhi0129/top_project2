b2b6a083031796cb814629b71e6cae0be9c028bc
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 17:06:26 +0200

    O3 vs Ofast new results

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -7,6 +7,3 @@ python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_-O3_
 python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_-Ofast_24.txt > ../speed_up/speed_up_100_-Ofast.txt
 python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_-Ofast_24.txt > ../speed_up/speed_up_500_-Ofast.txt
 python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_-Ofast_24.txt > ../speed_up/speed_up_1000_-Ofast.txt
-
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_block64.txt > ../speed_up/speed_up_500_block64.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_block32.txt > ../speed_up/speed_up_500_block32.txt
