5d79ab8672acadfad29c80cd3b5b13bec8a4eb9c
yizhi0129 <bailiyunzhong@gmail.com>
2024-05-05 16:22:43 +0200

    results flag -O3 -Ofast

--- scripts/run_p.sh
+++ scripts/run_p.sh
@@ -8,14 +8,14 @@ cmake .
 make
 
 j=24
-perf record ./my_executable ../config.txt ../results/res_100_-O3_$j.txt
-perf report > ../speed_up/perf_100_-O3_$j.txt
+perf record ./my_executable ../config.txt ../results/res_100_-Ofast_$j.txt
+perf report > ../speed_up/perf_100_-Ofast_$j.txt
 
-perf record ./my_executable ../config_500.txt ../results/res_500_-O3_$j.txt
-perf report > ../speed_up/perf_500_-O3_$j.txt
+perf record ./my_executable ../config_500.txt ../results/res_500_-Ofast_$j.txt
+perf report > ../speed_up/perf_500_-Ofast_$j.txt
 
-perf record ./my_executable ../config_1000.txt ../results/res_1000_-O3_$j.txt
-perf report > ../speed_up/perf_1000_-O3_$j.txt
+perf record ./my_executable ../config_1000.txt ../results/res_1000_-Ofast_$j.txt
+perf report > ../speed_up/perf_1000_-Ofast_$j.txt
 
 
 cd ../scripts
