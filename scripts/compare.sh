a7b4bc1e4432f2682c665a7b70e12131777197cc
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-07 12:05:15 +0200

    block size ijk better for dim 100 worse for 1000

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for i in 1 2 4 8 16 24 48
-do
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_o_$i.txt > ../speed_up/scal_100_o_$i.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_o_$i.txt > ../speed_up/scal_500_o_$i.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_o_$i.txt > ../speed_up/scal_1000_o_$i.txt
-done
+i=24
+python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_ijk_$i.txt > ../speed_up/sp_100_ijk_$i.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_ijk_$i.txt > ../speed_up/sp_500_ijk_$i.txt
+python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_ijk_$i.txt > ../speed_up/sp_1000_ijk_$i.txt
